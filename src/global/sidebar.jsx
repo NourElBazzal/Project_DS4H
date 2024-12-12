727e81d3a596be49e748d3e1b666e72e690ec57c
Maxime <jinjamb09@gmail.com>
2024-12-12 15:12:15 +0100

    improving sidebar loading (load only once and stock it in cache)

--- src/global/sidebar.jsx
+++ src/global/sidebar.jsx
@@ -2,73 +2,73 @@ import React, { useState, useEffect } from "react";
 import { ProSidebar, Menu, MenuItem, SidebarHeader, SidebarFooter, SidebarContent } from "react-pro-sidebar";
 import "react-pro-sidebar/dist/css/styles.css";
 import { Link } from "react-router-dom";
-import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
-import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
-import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
-import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
-import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
-import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
-import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
-import BarChartOutlinedIcon from "@mui/icons-material/BarChartOutlined";
-import PieChartOutlineOutlinedIcon from "@mui/icons-material/PieChartOutlineOutlined";
-import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
-import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
 import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
-import axios from "axios";  // Importing axios for API requests
+import axios from "axios";
 
 const Sidebar = () => {
   const [collapsed, setCollapsed] = useState(false);
-  const [views, setViews] = useState([]); // State to store the views fetched from the backend
+  const [views, setViews] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    // Fetch the views data from the backend API
     const fetchViews = async () => {
       try {
-        const response = await axios.get('https://dronic.i3s.unice.fr:8080/?username=user&password=test');
-        setViews(response.data.views);  // Set the fetched views into state
+        const cachedViews = localStorage.getItem('views');
+        if (cachedViews) {
+          setViews(JSON.parse(cachedViews));
+          setLoading(false);
+        } else {
+          const response = await axios.get('https://dronic.i3s.unice.fr:8080/?username=user&password=test');
+          localStorage.setItem('views', JSON.stringify(response.data.views));
+          setViews(response.data.views);
+          setLoading(false);
+        }
       } catch (error) {
         console.error('Error fetching views:', error);
-        console.log(error);
+        setLoading(false);
       }
     };
 
-    fetchViews(); // Call the function to fetch views when the component mounts
-  }, []); // Empty dependency array to ensure it runs only once after initial render
+    fetchViews();
+  }, []);
+
+  if (loading) {
+    return <div className="spinner">Loading...</div>;
+  }
 
   return (
-    <ProSidebar collapsed={collapsed}>
-      <SidebarHeader>
-        <div style={{ padding: "20px", textAlign: "center" }}>
-          <MenuOutlinedIcon
-            style={{ cursor: "pointer", fontSize: "24px" }}
-            onClick={() => setCollapsed(!collapsed)}
-          />
-          {!collapsed && (
-            <div style={{ marginTop: "10px", fontFamily:"'IBM Plex Sans', sans-serif" }}>
-              <h3 style={{ margin: 0, color: "rgb(48, 109, 173)", fontSize:"30px"}}>I3S</h3>
-            </div>
-          )}
-        </div>
-      </SidebarHeader>
+      <ProSidebar collapsed={collapsed}>
+        <SidebarHeader>
+          <div style={{ padding: "20px", textAlign: "center" }}>
+            <MenuOutlinedIcon
+                style={{ cursor: "pointer", fontSize: "24px" }}
+                onClick={() => setCollapsed(!collapsed)}
+            />
+            {!collapsed && (
+                <div style={{ marginTop: "10px", fontFamily:"'IBM Plex Sans', sans-serif" }}>
+                  <h3 style={{ margin: 0, color: "rgb(48, 109, 173)", fontSize:"30px"}}>I3S</h3>
+                </div>
+            )}
+          </div>
+        </SidebarHeader>
 
-      <SidebarContent>
-        <Menu iconShape="circle">
-          {/* Génère dynamiquement en fonction des vues trouvées sur le backend */}
-          {views.map((view, index) => (
-              <MenuItem key={index} icon={<MenuOutlinedIcon />}>
-                {view.name}
-                <Link to={`/information/${index}`} /> {/* Lis selon les vues trouvées */}
-              </MenuItem>
-          ))}
-        </Menu>
-      </SidebarContent>
+        <SidebarContent>
+          <Menu iconShape="circle">
+            {views.map((view, index) => (
+                <MenuItem key={index} icon={<MenuOutlinedIcon />}>
+                  {view.name}
+                  <Link to={`/information/${index}`} />
+                </MenuItem>
+            ))}
+          </Menu>
+        </SidebarContent>
 
-      <SidebarFooter style={{ textAlign: "center" }}>
-        <div style={{ padding: "20px 10px" }}>
-          <h6 style={{ color: "rgb(41, 82, 126)" }}>Footer Content</h6>
-        </div>
-      </SidebarFooter>
-    </ProSidebar>
+        <SidebarFooter style={{ textAlign: "center" }}>
+          <div style={{ padding: "20px 10px" }}>
+            <h6 style={{ color: "rgb(41, 82, 126)" }}>Footer Content</h6>
+          </div>
+        </SidebarFooter>
+      </ProSidebar>
   );
 };
 
