7074ed6393525e296b3da3649293cebddf19f5d3
TriForMine <quentin@triformine.dev>
2024-12-18 15:48:01 +0100

    disable dark theme

--- src/global/App.jsx
+++ src/global/App.jsx
@@ -4,12 +4,16 @@ import {Outlet} from "react-router-dom";
 import {createTheme} from "@mui/material";
 import {router} from "./router";
 
-const theme = createTheme(({
 
-}))
+const theme = createTheme({
+    cssVariables: true,
+    colorSchemes: {
+        dark: false
+    }
+})
 
 export default function App() {
-    return  <AppProvider router={router} branding={{
+    return  <AppProvider router={router} theme={theme} branding={{
         title: '',
         logo: <img src="/logo.svg" alt="I3S" width={"100%"} height={"100%"} color={"inherit"} />,
         homeUrl: '/home',
